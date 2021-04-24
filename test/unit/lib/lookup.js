26aa117453788f53a23a9f0fedd1134df83415fc
Justin Schwartzbeck <justinmschw@gmail.com>
2021-04-24 06:05:20 -0500

    Use sqlite for acl db

--- test/unit/lib/lookup.js
+++ test/unit/lib/lookup.js
@@ -1,40 +1,42 @@
 'use strict'
 
-const LookupTree = require('../../../lib/lookup');
+const Lookup = require('../../../lib/lookup');
 const expect = require('chai').expect;
 
-describe('Tree', function() {
+describe('Lookup', function() {
+    let lookup = new Lookup({aclDatabaseFile: '/tmp/acls.db'});
     describe('addHostName', function() {
+        beforeEach(async function() {
+            await lookup.init();
+        });
+        afterEach(async function() {
+            await lookup.cleanup();
+            await lookup.close();
+        });
         it('valid', async function() {
-            let tree = new LookupTree();
-            tree.addHostName('subdomain.mydomain.com', 'myCategory');
-            expect(Object.keys(tree.children).length).gt(0);
-            const entry = tree.lookupHostName('subdomain.mydomain.com');
-            expect(entry.category).eql('myCategory');
+            await lookup.addHostName('subdomain.mydomain.com', 'myCategory');
+            const result = await lookup.lookupHostName('subdomain.mydomain.com', 'myCategory');
+            expect(result).to.not.be.undefined;
         });
         it('duplicate', async function() {
-            let tree = new LookupTree();
-            tree.addHostName('subdomain.mydomain.com', 'myCategory');
-            let before = Object.keys(tree.children).length;
-            tree.addHostName('subdomain.mydomain.com', 'myCategory');
-            let after = Object.keys(tree.children).length;
-            expect(before).eql(after);
+            lookup.addHostName('subdomain.mydomain.com', 'myCategory');
+            lookup.addHostName('subdomain.mydomain.com', 'myCategory');
+            const result = await lookup.lookupHostName('subdomain.mydomain.com', 'myCategory');
+            expect(result).to.not.be.undefined;
         });
         it('empty', async function() {
-            let tree = new LookupTree();
             let error = null;
             try {
-                tree.addHostName('', 'myCategory');
+                await lookup.addHostName('', 'myCategory');
             } catch (err) {
                 error = err;
             }
             expect(error).is.not.null;
         });
         it('malformed', async function() {
-            let tree = new LookupTree();
             let error = null;
             try {
-                tree.addHostName('.subdomain.com', 'myCategory');
+                await lookup.addHostName('.subdomain.com', 'myCategory');
             } catch (err) {
                 error = err;
             }
@@ -42,50 +44,33 @@ describe('Tree', function() {
         });
     });
     describe('lookupHostName', function() {
+        beforeEach(async function() {
+            await lookup.init();
+        });
+        afterEach(async function() {
+            await lookup.cleanup();
+            await lookup.close();
+        });
         it('subdomain', async function() {
-            let tree = new LookupTree();
-            tree.addHostName('mydomain.com', 'myCategory');
-            expect(Object.keys(tree.children).length).gt(0);
-            const entry = tree.lookupHostName('subdomain.mydomain.com');
-            expect(entry.category).eql('myCategory');
-        });
-        it('empty', async function() {
-            let tree = new LookupTree();
-            let error = null;
-            try {
-                tree.lookupHostName('');
-            } catch (err) {
-                error = err;
-            }
-            expect(error).is.not.null;
-        });
-        it('malformed', async function() {
-            let tree = new LookupTree();
-            let error = null;
-            try {
-                tree.lookupHostName('.subdomain.mydomain.com');
-            } catch (err) {
-                error = err;
-            }
-            expect(error).is.not.null;
+            await lookup.addHostName('mydomain.com', 'myCategory');
+            const entry = await lookup.lookupHostName('subdomain.mydomain.com', 'myCategory');
+            expect(entry.categoryText).eql('myCategory');
         });
     });
-    describe('addNode', function() {
-       it('valid', function() {
-           let tree = new LookupTree();
-           tree.addNode('id1');
-           expect(Object.keys(tree.children).length).gt(0);
-       });
-       it('duplicate', function() {
-           let tree = new LookupTree();
-           tree.addNode('id1');
-           let error = null;
-           try {
-               tree.addNode('id1');
-           } catch (err) {
-               error = err;
-           }
-           expect(error).is.not.null;
-       });
+    describe('loadShallaDomains', function() {
+        beforeEach(async function() {
+            await lookup.init();
+        });
+
+        afterEach(async function() {
+            await lookup.cleanup();
+            await lookup.close();
+        });
+        /*it('valid', async function() {
+            await lookup.loadDomainsFile(`${process.env.PWD}/test/unit/data/domains`, 'myCategory');
+            expect(Object.keys(lookup.entries).length).eql(3);
+            const val = lookup.lookupHostName('domain1.com');
+            expect(val).to.not.be.null;
+        });*/
     });
 });
