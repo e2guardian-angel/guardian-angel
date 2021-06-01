6db5060d31bf54498494446c123d6464c1f247a9
Justin Schwartzbeck <justinmschw@gmail.com>
2021-05-31 21:11:53 -0500

    Fixes tls mount

--- test/integration/lib/controller.js
+++ test/integration/lib/controller.js
@@ -38,21 +38,55 @@ describe('/lib/controller', function() {
             const pods = await controller.kubeGet(controller.paths.kube.pods);
             pods.body.items.forEach(pod => {
                 if (pod.metadata.name.startsWith('webfilter')) {
-                    // check to make sure that transocks is not deployed
-                    let transocksContainer = pod.spec.containers.filter(container => container.name === 'transocks');
-                    let e2guardianContainer = pod.spec.containers.filter(container => container.name === 'e2guardian');
-                    expect(transocksContainer.length).eql(0);
-                    expect(e2guardianContainer.length).eql(0);
+                    // Only squid container should be deployed
+                    expect(pod.spec.containers.length).eql(1);
+                    expect(pod.spec.containers[0].name === 'squid')
+                }
+            });
+            const services = await controller.kubeGet(controller.paths.kube.services);
+            services.body.items.forEach(service => {
+                if (service.metadata.name.startsWith('webfilter')) {
+                    // check to make sure transocks port is not open
+                    expect(service.spec.ports.length).eql(1);
+                    expect(service.spec.ports[0].name === 'squid');
                 }
             });
         });
         it('ssl enabled', async function() {
             try {
-
+                let sslConfig = new Config({});
+                sslConfig.sslBumpEnabled = true;
+                // Pull kubeconfig
+                console.info('Fetching kube data...');
+                await controller.getKubeData();
+                controller.setConfig(sslConfig);
+                console.info('Deploying all objects...');
+                await controller.deployAll();
+                console.info('Done');
             } catch(err) {
                 console.error(`Failed test: ${err.message}`);
                 expect(err).to.be.null;
             }
+            // Get all kube deployments and make sure they are as expected
+            const pods = await controller.kubeGet(controller.paths.kube.pods);
+            pods.body.items.forEach(pod => {
+                if (pod.metadata.name.startsWith('webfilter')) {
+                    // Only squid container should be deployed
+                    expect(pod.spec.containers.length).eql(2);
+                    let squidContainer = pod.spec.containers.filter(pod => pod.name.startsWith('squid'));
+                    let e2gContainer = pod.spec.containers.filter(pod => pod.name.startsWith('e2guardian'));
+                    expect(squidContainer.length).gt(0);
+                    expect(e2gContainer.length).gt(0);
+                }
+            });
+            const services = await controller.kubeGet(controller.paths.kube.services);
+            services.body.items.forEach(service => {
+                if (service.metadata.name.startsWith('webfilter')) {
+                    // check to make sure transocks port is not open
+                    expect(service.spec.ports.length).eql(1);
+                    expect(service.spec.ports[0].name === 'squid');
+                }
+            });
         });
     });
 });
