e8c76c986d115576b326ada77ce54af8f08f04dd
Justin Schwartzbeck <justinmschw@gmail.com>
2021-06-19 06:52:27 -0500

    Init dashboard, temporarily disable content security

--- routes/ui/dashboard.js
+++ routes/ui/dashboard.js
@@ -3,7 +3,20 @@ const ejs = require('ejs');
 const fs = require('fs');
 
 function cert(req, res) {
-    res.status(200).send(ejs.render(fs.readFileSync(`${__dirname}/views/dashboard.html`, 'utf-8')));
+    const overview = fs.readFileSync(`${__dirname}/views/overview-content.html`);
+    const profiles = fs.readFileSync(`${__dirname}/views/profiles-content.html`);
+    const filter = fs.readFileSync(`${__dirname}/views/filter-content.html`);
+    const categories = fs.readFileSync(`${__dirname}/views/categories-content.html`);
+    const phrases = fs.readFileSync(`${__dirname}/views/phrases-content.html`);
+    const advanced = fs.readFileSync(`${__dirname}/views/advanced-content.html`);
+    res.status(200).send(ejs.render(fs.readFileSync(`${__dirname}/views/dashboard.html`, 'utf-8'), {
+        overviewContent: overview,
+        profilesContent: profiles,
+        filterContent: filter,
+        categoriesContent: categories,
+        phrasesContent: phrases,
+        advancedContent: advanced
+    }));
 }
 
 module.exports = cert;
