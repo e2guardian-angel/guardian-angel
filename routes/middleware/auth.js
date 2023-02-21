1b3d04c737110070ec5ca851dad2bde889cee34f
Justin Schwartzbeck <justinmschw@gmail.com>
2023-02-20 23:34:55 -0600

    parse auth header in try block

--- routes/middleware/auth.js
+++ routes/middleware/auth.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 const secret = process.env.JWT_SECRET
 
 function auth(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
     try {
+        const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, secret);
         next();
     } catch (err) {
-        res.status(401).send('Invalid token')
+        res.status(401).send('Invalid/missing token')
     }
 }
 
